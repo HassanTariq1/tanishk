226a6a1ebaf3deda4de848ba931ba8d10c23f8e9
Hassan <mughalhassan467@gmail.com>
2018-12-14 14:33:07 +0500

    HM

--- app/src/main/java/com/itpvt/tanishk/MainActivity.java
+++ app/src/main/java/com/itpvt/tanishk/MainActivity.java
@@ -16,7 +16,19 @@ import android.telephony.PhoneNumberUtils;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, BaseSliderView.OnSliderClickListener {
+
+
+    SliderLayout sliderLayout;
+    HashMap<String, Integer> HashMapForURL ;
+    //int[] images={  R.drawable.ban, R.drawable.bann, R.drawable.bannn};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +69,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        AddImagesUrlOnline();
+
     }
 
     @Override
@@ -109,6 +124,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+    private void AddImagesUrlOnline()
+
+    {
+
+        HashMapForURL = new HashMap<String, Integer>();
+
+      //  HashMapForURL.put(" ", R.drawable.ban);
+      //  HashMapForURL.put("  ", R.drawable.bann);
+       // HashMapForURL.put("   ", R.drawable.bannn);
+
+        callSlider();
+
+    }
+    private void callSlider() {
+
+        for(String name : HashMapForURL.keySet()){
+
+            TextSliderView textSliderView = new TextSliderView(MainActivity.this.getApplicationContext());
+
+            textSliderView
+                    .description(name)
+                    .image(HashMapForURL.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            textSliderView.bundle(new Bundle());
+
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            sliderLayout.addSlider(textSliderView);
+        }
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.DepthPage);
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        sliderLayout.setCustomAnimation(new DescriptionAnimation());
+        sliderLayout.setDuration(8000);
     }
 //    public static Intent openFacebook(Context context) {
 //
